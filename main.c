adc76d833a2b47b95d31aeb0bb49db6f3abca5d8
Alessandro <aleangelozi@gmail.com>
2022-07-09 22:23:19 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*This program takes a value in minutes via terminal,
+and convert this value to days and years format.
+
+The program should display both the inputed and the converted value.
+
+Program variables: minutes, minutes in year, Years, Days.
+*/
+
+int main()
+{
+    int minutes = 0;
+    double minutesInDay = 1440.0;
+    double minutesInYear = 525600.0;
+    double days = 0.0;
+    double years = 0.0;
+
+    printf("Please, enter a integer value\n");
+    scanf("%d", &minutes);
+
+    days = (double)minutes / minutesInDay;
+    years = ((double)minutes / minutesInYear);
+
+    printf("%d is approximately %lf Days, %lf Years", minutes, days, years);
+    return 0;
+}
